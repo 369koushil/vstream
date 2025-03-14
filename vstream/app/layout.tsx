e1346e8910927156730994bd95e19c062ee61824
Koushil C <koushil463@gmail.com>
2025-03-15 02:11:25 +0530

    resolving tost event

--- vstream/app/layout.tsx
+++ vstream/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "./components/Provider";
+import { Toaster } from "@/components/ui/sonner"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       ><Provider>
+        <Toaster  position="bottom-right" richColors duration={3000} />
           {children}
         </Provider>
       </body>
